865528727773660673c4fd140c8b844fffe73d8b
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-15 10:56:12 +0500

    importing and creaing custom packages

---
+++ 03_Importing_Modules_Packages/packages/__init__.py
@@ -0,0 +1,2 @@
+# it is a spacial file, used in python to define packages and initialize their namespaces
+# it is used to import modules and packages 
---
+++ 03_Importing_Modules_Packages/packages/math.py
@@ -0,0 +1,3 @@
+def add(a,b):
+    '''This function take two arguments and sum them '''
+    return a + b
